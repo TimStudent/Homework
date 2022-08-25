d01268e3cf912ae9c9700e2d41a7074157751c3f
consultant <kanimize@gmail.com>
2022-08-25 12:18:00 -0400

    commit

---
+++ app/src/main/java/com/example/timhelloworld/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.timhelloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
